66b1737a1d2483ab58aaef827615ed099260e2b1
sophie22 <rzsozso@gmail.com>
2022-05-07 16:13:58 +0100

    read in file contents

--- genes_coverage.py
+++ genes_coverage.py
@@ -6,11 +6,18 @@ import pandas as pd # v1.3.4
 
 ### Read inputs from  the command line
 # sambamba output file (tsv)
-
+sambamba_file = sys.argv[1]
 # coverage threshold (default to 30)
-
+if len(sys.argv) > 2:
+    coverage_threshold = sys.argv[2]
+else:
+    coverage_threshold = "30"
+coverage_column = "percentage" + coverage_threshold
 
 ### Load sambamba output file contents into a DataFrame
+sambamba_df = pd.read_csv(sambamba_file, sep='\t')
+print(sambamba_df.columns)
+print(sambamba_df.head())
 
 ### Identify exons with less than 100% coverage at 30x
 
