14960e37cb8b365bc4a157a46733686de84447b3
sophie22 <rzsozso@gmail.com>
2022-05-07 16:11:10 +0100

    pseudo code

---
+++ genes_coverage.py
@@ -0,0 +1,19 @@
+# Python 3.8
+
+# Import libraries/packages for use in the code
+import sys
+import pandas as pd # v1.3.4
+
+### Read inputs from  the command line
+# sambamba output file (tsv)
+
+# coverage threshold (default to 30)
+
+
+### Load sambamba output file contents into a DataFrame
+
+### Identify exons with less than 100% coverage at 30x
+
+### Identify unique genes with at least one exon with suboptimal coverage
+
+### Write gene symbols with suboptimal coverage to file
